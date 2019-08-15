2b64ec3f4cde5bebc5c134ab98d59338082398c4
PatrykWasowski <patryk.wasowski10@gmail.com>
2019-08-15 23:12:10 +0200

    Implemented range input for changing size of cells

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: 20,
+      size: 60,
       dimension: 9
     }
   }
@@ -16,9 +16,6 @@ class App extends React.Component {
       ...this.state,
       size: e.target.value
     });
-
-    
-    console.log(this.state);
   }
 
   dimensionChangeHandler = (e) => {
@@ -28,16 +25,15 @@ class App extends React.Component {
         dimension: e.target.value
       })
     }
-    console.log(this.state);
   }
 
 
   render() {
     return (
     <div className="App">
+      <input type="number" id="dimension" min="1" max="20" onChange={this.dimensionChangeHandler} defaultValue="9" />
+      <input type="range" id="size" min="10" max="100" step="2" onChange={this.sizeChangeHandler} defaultValue="60" />
       <Board size={this.state.size} dimension={this.state.dimension} />
-      <input type="number" min="1" max="20" id="dimension" onChange={this.dimensionChangeHandler} defaultValue="9"/>
-      <input type="range" id="size" min="10" max="100" step="5" onChange={this.sizeChangeHandler}/>
     </div>
     )
   }
--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,41 +1,35 @@
 import React from 'react';
-import { Component } from 'react';
+import './Board.css';
 
-class Board extends Component {    
-    tableStyle = {
-        backgroundColor: '#f8f5ed',
-        margin: 'auto',
-        marginTop: '100px',
-        border: '2px black solid',
-        boxShadow: '0px 0px 10px white'
-    }
-    
-    cellStyle = {
-        height: this.props.size,
-        width: this.props.size,
-        border: '2px #43464B solid'
-    }
-    
+class Board extends React.Component {    
     handleCellClick = (e) => {
         alert(e.target.id);
     }
     
     populateTable = () => {
+        const cellStyle = {
+            height: this.props.size + 'px',
+            width: this.props.size + 'px',
+            border: '2px #43464B solid'
+        };
+
         let rows = [];
         for (let i = 0; i < this.props.dimension; i++) {
-            let children = [];
+            let cells = [];
             for (let j = 0; j < this.props.dimension; j++) {
-                children.push(<td key={i + " " + j} style={this.cellStyle} onMouseUp={this.handleCellClick}></td>)
+                cells.push(<td id={i + " " + j} key={i+j} style={cellStyle} onMouseUp={this.handleCellClick}></td>)
             }
-            rows.push(<tr key={i}>{children}</tr>)
+            rows.push(<tr key={i}>{cells}</tr>)
         }
+        console.log(cellStyle);
+
         return rows;
     }
     
     render() {
         return (
         <div>
-            <table id = "board" style={this.tableStyle}>
+            <table id = "board">
                 <tbody>
                     { this.populateTable() }
                 </tbody>
